deb418f844cae10a582bea707019a97b22270486
oneyear <690557774@qq.com>
2016-07-19 22:28:08 +0800

    oneyear! let me get up!

---
+++ src/dataReadFromDatabase/moduleTest.java
@@ -0,0 +1,17 @@
+package dataReadFromDatabase;
+//import java.util.Date;
+/**
+ * Created by jin on 2016/7/19.
+ */
+
+
+
+
+public class moduleTest {
+    public static void main(String[] args)
+    {
+//        System.out.printf("");System.out.print("hello world");
+    System.out.print("hello world");
+
+    }
+}
