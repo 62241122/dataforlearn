8ff6b5899c553096f7e8715ba1a2cfef201df643
oneyear <690557774@qq.com>
2016-07-19 22:28:41 +0800

    oneyear! let me get up!

---
+++ src/dataReadFromDatabase/vo.java
@@ -0,0 +1,15 @@
+package dataReadFromDatabase;
+
+import java.util.Date;
+
+/**
+ * Created by jin on 2016/7/19.
+ */
+public class vo {
+    private int devicenum;
+    private String ename;
+    private String job;
+    private Date hiredate;
+    private float sal;
+
+}
